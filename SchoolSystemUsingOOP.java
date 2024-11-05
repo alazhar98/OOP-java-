f050b691a9a62d4010fcf5823dcfd8574f6c495a
alazhar <alazhar9814@gmail.com>
2024-11-05 10:50:24 +0400

    comment

--- SchoolSystemUsingOOP.java
+++ SchoolSystemUsingOOP.java
@@ -77,11 +77,11 @@ public class SchoolSystemUsingOOP {
         }
     }
     // ask the user how many school he/ she want to add
-    
+    // ask the user to enter school name
     private static void addSchools(Map<String, Map<String, Object>> schools, Scanner scanner) {
         System.out.print("How many schools do you want to add? ");
         int numberOfSchools = scanner.nextInt();
-        scanner.nextLine(); // Consume newline
+        scanner.nextLine();
 
         for (int i = 0; i < numberOfSchools; i++) {
             System.out.print("Enter school name: ");
