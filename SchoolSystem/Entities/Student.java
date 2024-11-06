623ba9ad0927d1f1e4e7ffc7a55aa1b375c10550
alazhar <alazhar9814@gmail.com>
2024-11-06 11:46:21 +0400

    this class Student

---
+++ SchoolSystem/Entities/Student.java
@@ -0,0 +1,9 @@
+package SchoolSystem.Entities;
+
+public class Student {
+    String id;
+    String name;
+    String grade;
+    Short age;
+    List<Subject> courses;
+}
---
+++ SchoolSystem/Entities/Subject.java
@@ -0,0 +1,5 @@
+package SchoolSystem.Entities;
+
+public class Subject {
+
+}
