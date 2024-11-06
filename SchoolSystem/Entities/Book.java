48812299900a5e188cd3a27bbf32356e220ae44d
alazhar <alazhar9814@gmail.com>
2024-11-06 11:43:21 +0400

    this class Book

---
+++ SchoolSystem/Entities/Book.java
@@ -0,0 +1,11 @@
+package SchoolSystem.Entities;
+
+import java.util.Date;
+public class Book {
+    String name;
+    String author;
+    Integer id;
+    Date yearOfPublishing;
+    Boolean isAvailable;
+
+}
