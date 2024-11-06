6c3bf16e193a958c082c20b50d9123fead1f5c1e
alazhar <alazhar9814@gmail.com>
2024-11-06 11:48:05 +0400

    this class Teacher

---
+++ SchoolSystem/Entities/Teacher.java
@@ -0,0 +1,10 @@
+package SchoolSystem.Entities;
+
+import java.util.List;
+
+public class Teacher {
+    String name;
+    String id;
+    Short yearsOfExperience;
+    List<Subject> subjectsExpertiseList;
+}
