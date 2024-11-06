6a67da4a7c2de74a2055e4781966d4a439db6bbb
alazhar <alazhar9814@gmail.com>
2024-11-06 11:45:17 +0400

    this class School

---
+++ SchoolSystem/Entities/School.java
@@ -0,0 +1,9 @@
+package SchoolSystem.Entities;
+
+public class School {
+    public String name;
+    public String address;
+    public List<Student> students;
+    public List<Teacher> teachers;
+    public Library library;
+}
