388cff2aea7e955ccda2f59f310b2b32163e5b9a
alazhar <alazhar9814@gmail.com>
2024-11-07 08:55:59 +0400

    commet

--- SchoolSystem/SchoolSystem.java
+++ SchoolSystem/SchoolSystem.java
@@ -4,21 +4,7 @@ import SchoolSystem.Services.*;
 
 public class SchoolSystem {
     public static void main(String[] args) {
-        BookService bookService = new BookService();
-        LibraryService libraryService = new LibraryService();
-        MarkService markService = new MarkService();
-        SchoolService schoolService = new SchoolService();
-        StudentService studentService = new StudentService();
-        SubjectService subjectService = new SubjectService();
-        TeacherService teacherService = new TeacherService();
 
-        System.out.println(schoolService);
-        System.out.println(studentService);
-        System.out.println(subjectService);
-        System.out.println(markService);
-        System.out.println(teacherService);
-        System.out.println(libraryService);
-        System.out.println(bookService);
     }
 
 }
