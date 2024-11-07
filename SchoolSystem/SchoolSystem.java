818b8360f1763bbf45eee20c4e72d53a135a32ba
alazhar <alazhar9814@gmail.com>
2024-11-07 11:45:59 +0400

    delete scanner

--- SchoolSystem/SchoolSystem.java
+++ SchoolSystem/SchoolSystem.java
@@ -9,7 +9,6 @@ import java.util.Scanner;
 public class SchoolSystem {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter School details");
         SchoolService.addSchool();
