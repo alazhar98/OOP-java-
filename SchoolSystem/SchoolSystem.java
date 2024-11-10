563589e57fca78fbb416dd7c897e9bfa4c82d303
alazhar <alazhar9814@gmail.com>
2024-11-10 08:28:09 +0400

    change the print statement in main class

--- SchoolSystem/SchoolSystem.java
+++ SchoolSystem/SchoolSystem.java
@@ -7,7 +7,7 @@ public class SchoolSystem {
     public static void main(String[] args) {
 
 
-        System.out.println("Enter School details");
+        System.out.println("***School details***");
         SchoolService.addSchool();
     }
 }
