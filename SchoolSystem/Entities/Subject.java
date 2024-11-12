e4c9b198b512d44b62a564ce1da81631eb0cb054
alazhar <alazhar9814@gmail.com>
2024-11-12 11:07:42 +0400

    geter and seter

--- SchoolSystem/Entities/Subject.java
+++ SchoolSystem/Entities/Subject.java
@@ -3,8 +3,31 @@ package SchoolSystem.Entities;
 import java.util.List;
 
 public class Subject {
-    public String name;
-    public Short creditHours;
-    public List<Mark> marks;
+    private String name;
+    private Short creditHours;
+    private List<Mark> marks;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Short getCreditHours() {
+        return creditHours;
+    }
+
+    public void setCreditHours(Short creditHours) {
+        this.creditHours = creditHours;
+    }
+
+    public List<Mark> getMarks() {
+        return marks;
+    }
+
+    public void setMarks(List<Mark> marks) {
+        this.marks = marks;
+    }
 }
