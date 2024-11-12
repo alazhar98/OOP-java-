55d06254c7be3143ce65ca32b21e6b32ce9667e7
alazhar <alazhar9814@gmail.com>
2024-11-12 11:07:28 +0400

    geter and seter

--- SchoolSystem/Entities/Student.java
+++ SchoolSystem/Entities/Student.java
@@ -3,9 +3,49 @@ package SchoolSystem.Entities;
 import java.util.List;
 
 public class Student {
-    public String id;
-    public String name;
-    public String grade;
-    public Short age;
+    private String id;
+    private String name;
+    private String grade;
+    private Short age;
     public List<Subject> courses;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    public Short getAge() {
+        return age;
+    }
+
+    public void setAge(Short age) {
+        this.age = age;
+    }
+
+    public List<Subject> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(List<Subject> courses) {
+        this.courses = courses;
+    }
 }
