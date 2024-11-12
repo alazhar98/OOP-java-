42e914b27802d20fee314f3ef541536c967b5362
alazhar <alazhar9814@gmail.com>
2024-11-12 12:02:06 +0400

    change them private

--- SchoolSystem/Entities/Book.java
+++ SchoolSystem/Entities/Book.java
@@ -1,12 +1,13 @@
 package SchoolSystem.Entities;
 
 import java.util.Date;
+
 public class Book {
-    public String name;
-    public String author;
-    public Integer id;
-    public String yearOfPublishing;
-    public Boolean isAvailable;
+    private String name;
+    private String author;
+    private Integer id;
+    private String yearOfPublishing;
+    private Boolean isAvailable;
 
     public String getName() {
         return name;
