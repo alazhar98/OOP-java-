3fcf5596dc0b3f3cb2a383643c556432ca0a4956
alazhar <alazhar9814@gmail.com>
2024-11-12 11:07:38 +0400

    geter and seter

--- SchoolSystem/Entities/Teacher.java
+++ SchoolSystem/Entities/Teacher.java
@@ -3,10 +3,40 @@ package SchoolSystem.Entities;
 import java.util.List;
 
 public class Teacher {
-    public String name;
-    public String id;
-    public Short yearsOfExperience;
-    public List<Subject> subjectsExpertiseList;
+    private String name;
+    private String id;
+    private Short yearsOfExperience;
+    private List<Subject> subjectsExpertiseList;
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Short getYearsOfExperience() {
+        return yearsOfExperience;
+    }
+
+    public void setYearsOfExperience(Short yearsOfExperience) {
+        this.yearsOfExperience = yearsOfExperience;
+    }
+
+    public List<Subject> getSubjectsExpertiseList() {
+        return subjectsExpertiseList;
+    }
+
+    public void setSubjectsExpertiseList(List<Subject> subjectsExpertiseList) {
+        this.subjectsExpertiseList = subjectsExpertiseList;
+    }
 }
