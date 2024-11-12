393e0ac96fad0c8244edf4559f98d3f8dcf6d327
alazhar <alazhar9814@gmail.com>
2024-11-12 11:07:33 +0400

    geter and seter

--- SchoolSystem/Entities/School.java
+++ SchoolSystem/Entities/School.java
@@ -3,9 +3,17 @@ package SchoolSystem.Entities;
 import java.util.List;
 
 public class School {
-    public String name;
+    private String name;
     public String address;
     public List<Student> students;
     public List<Teacher> teachers;
     public Library library;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
